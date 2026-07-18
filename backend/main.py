74f6d7e06945ad99a6682c91e930274372e6a19b
Shiv <nandshiv31@gmail.com>
2026-07-18 18:37:59 +0530

    Postgres connection via SQLAlchemy

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from database import get_db
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return{"message" : "backend is alive"}
+    return {"message": "Backend is alive"}
+
+@app.get("/db-check")
+def db_check(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+    return {"database": "connected", "result": result.scalar()}
